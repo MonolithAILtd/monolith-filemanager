76162218e465f0925ec8e74dfaf2ad7f660e37da
maxwell flitton <maxwell@monolithai.com>
2021-05-10 17:02:33 +0100

    adding gmsh

--- monolith_filemanager/file/__init__.py
+++ monolith_filemanager/file/__init__.py
@@ -28,6 +28,8 @@ class FileMap(dict, metaclass=Singleton):
         :return: None
         """
         if self.bindings_imported is False:
+            from monolith_filemanager.file.gmsh_file import GmshFile
+            self.add_binding(file_object=GmshFile)
             from monolith_filemanager.file.hdf5_file import Hdf5File
             self.add_binding(file_object=Hdf5File)
             from monolith_filemanager.file.json_file import JSONFile
---
+++ monolith_filemanager/file/gmsh_file.py
@@ -0,0 +1,50 @@
+from typing import Any, Union
+
+import tempfile
+
+from .base import File
+from ..path import FilePath
+
+
+class GmshFile(File):
+    """
+    This is a class for managing the reading and writing of stl objects.
+    """
+    SUPPORTED_FORMATS = ['brep', 'stp', 'step', 'igs', 'iges']
+
+    def __init__(self, path: Union[str, FilePath]) -> None:
+        """
+        The constructor for the CustomDataFile class.
+
+        :param path: (str/FilePath) path to the file
+        """
+        super().__init__(path=path)
+
+    def read(self, **kwargs) -> Any:
+        """
+        Gets data from file defined by the file path.
+
+        :return: using tempfile as a buffer to converting a CAD file to a mesh
+        """
+        import gmsh
+        # if we initialize with sys.argv it could be anything
+        gmsh.initialize()
+        gmsh.option.setNumber("General.Terminal", 1)
+        gmsh.model.add('Surface_Mesh_Generation')
+        gmsh.open(self.path)
+
+        # create a temporary file for the results
+        out_data = tempfile.NamedTemporaryFile(suffix='.stl', delete=False)
+        out_data.close()
+
+        return out_data
+
+    def write(self, data: Any) -> None:
+        """
+        Writes data to file.
+
+        :param data: (python object) data to be written to file
+        :return: None
+        """
+
+        data.save(self.path)
