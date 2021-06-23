d8df9068c30003f4dd8c3b6b4b78d99b018529af
Owen Walton <owen@monolithai.com>
2021-06-23 11:38:11 +0100

    test change

--- tests/file/test_map.py
+++ tests/file/test_map.py
@@ -28,32 +28,32 @@ class TestFileMap(TestCase):
         mock_init.return_value = None
         test = FileMap()
         mock_file_object = MagicMock()
-        mock_file_object.SUPPORTED_FORMATS = ["ext1", "ext2"]
+        mock_file_object.SUPPORTED_FORMATS = ["csv", "txt"]
         test.add_binding(file_object=mock_file_object)
 
         expected_outcome = {
-            "ext1": mock_file_object,
-            "ext2": mock_file_object
+            "csv": mock_file_object,
+            "txt": mock_file_object
         }
 
         self.assertEqual(expected_outcome, test)
 
         mock_file_object_two = MagicMock()
-        mock_file_object_two.SUPPORTED_FORMATS = ["ext3", "ext4"]
+        mock_file_object_two.SUPPORTED_FORMATS = ["pdf", "pickle"]
 
         test.add_binding(file_object=mock_file_object_two)
 
         expected_outcome = {
-            "ext1": mock_file_object,
-            "ext2": mock_file_object,
-            "ext3": mock_file_object_two,
-            "ext4": mock_file_object_two
+            "csv": mock_file_object,
+            "txt": mock_file_object,
+            "pdf": mock_file_object_two,
+            "pickle": mock_file_object_two
         }
 
         self.assertEqual(expected_outcome, test)
 
         mock_file_object_three = MagicMock()
-        mock_file_object_three.SUPPORTED_FORMATS = ["ext1"]
+        mock_file_object_three.SUPPORTED_FORMATS = ["txt"]
 
         with self.assertRaises(Exception):
             test.add_binding(file_object=mock_file_object_three)
