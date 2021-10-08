da90bad6b2d4b545febb6aa47494b310e443daec
Owen Walton <owen@monolithai.com>
2021-10-08 17:07:28 +0100

    added RequirementsManager class to setup.py to handle install_requires etc for Pipfile

--- setup.py
+++ setup.py
@@ -9,6 +9,65 @@ from setuptools.command.build_py import build_py as build_py_orig
 # dist.Distribution().fetch_build_eggs(['Cython==0.29'])
 # from Cython.Build import cythonize
 
+extras_packages = {
+    "flask": ["flask", "tensorflow", "boto3"],
+    "3d": ["pyvista", "gmsh"],
+    "matlab": ["scipy"]
+}
+
+
+class RequirementsManager:
+    def __init__(self):
+        with open("./Pipfile", "r") as pipfile:
+            self.pipfile = pipfile.read().split("\n")
+        self.package_index = self.pipfile.index("[packages]")
+        self.dev_package_index = self.pipfile.index("[dev-packages]")
+        self.simple_package_dict = {}
+        self.complex_package_dict = {}
+
+    def parse_complex_pack(self, package_str: str):
+        clean_list = [text.replace('"', '').replace("{", "").replace("}", "") for text in package_str.split(" = ")]
+        stripped_list = [item.strip() for sublist in [x.split(",") for x in clean_list] for item in sublist]
+        clean_package_str = stripped_list[0] + stripped_list[stripped_list.index("extras")+1] + \
+                            stripped_list[stripped_list.index("version")+1]
+        return clean_package_str
+
+    def set_package_dicts(self):
+        for package in self.get_simple_formatted_requirements():
+            name = package.split("==")[0]
+            version = package.split("==")[-1]
+            self.simple_package_dict[name] = version
+
+        for package in self.get_complex_formatted_requirements():
+            name = package.split("==")[0]
+            version = package.split("==")[-1]
+            self.complex_package_dict[name] = version
+
+    def get_simple_formatted_requirements(self):
+        return list(map(lambda x: x.replace(' = "', '')[:-1], self.simple_packages))
+
+    def get_complex_formatted_requirements(self):
+        return list(map(self.parse_complex_pack, self.complex_packages))
+
+    def get_loosened_requirements(self, filter: list = None):
+        if self.simple_package_dict != {}:
+            if filter:
+                return [f"{name}>={version}" for name, version in {**self.simple_package_dict,
+                                                                   **self.complex_package_dict}.items() if name in filter]
+            else:
+                return [f"{name}>={version}" for name, version in {**self.simple_package_dict,
+                                                                   **self.complex_package_dict}.items()]
+
+    @property
+    def simple_packages(self):
+        return [package for package in self.pipfile[self.package_index + 1:self.dev_package_index - 1] if
+                package.split(" = ")[1].startswith('"')]
+
+    @property
+    def complex_packages(self):
+        return [package for package in self.pipfile[self.package_index + 1:self.dev_package_index - 1] if
+                package.split(" = ")[1].startswith('{')]
+
 
 class CustomBuildPy(build_py_orig):
     """
@@ -30,6 +89,13 @@ directives = {
     'always_allow_keywords': True
 }
 
+reqs = RequirementsManager()
+reqs.set_package_dicts()
+
+# handle generation of install_requires filtered packages
+extra_packs = [item for sublist in extras_packages.values() for item in sublist]
+install_requires = [pack.split(">=")[0] for pack in reqs.get_loosened_simple_requirements() if pack not in extra_packs]
+
 setuptools.setup(
     name="monolith_filemanager",
     version=version,
@@ -39,19 +105,25 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MonolithAILtd/monolith-filemanager",
-    install_requires=[
-        "h5py<2.11.0,>=2.10.0",
-        "joblib>=0.10.0",
-        "numpy>=1.19.0",
-        "pandas>=1.1.5",
-        "PyYAML>=5.3",
-        "globre>=0.1.5",
-        "dill>=0.2.8"
-    ],
+    # install_requires=[
+    #     "h5py<2.11.0,>=2.10.0",
+    #     "joblib>=0.16.0",
+    #     "numpy>=1.16.4",
+    #     "pandas>=0.25.1",
+    #     "PyYAML>=5.1.2",
+    #     "globre>=0.1.5",
+    #     "dill>=0.2.9"
+    # ],
+    install_requires=install_requires,
+    # extras_require={
+    #     'flask': ["flask>=1.1.2", "tensorflow>=2.2.0", "boto3>=1.10.5"],
+    #     '3d': ["pyvista>=0.24.2", "gmsh>=4.8.4"],
+    #     'matlab': ["scipy>=1.4.1"]
+    # },
     extras_require={
-        'flask': ["Flask>=1.0.0", "tensorflow>=2.1.0", "boto3>=1.16.43"],
-        '3d': ["pyvista>=0.29.0", "gmsh>=4.2.0"],
-        'matlab': ["scipy>=1.4.1"]
+        'flask': reqs.get_loosened_simple_requirements(filter=extras_packages["flask"]),
+        '3d': reqs.get_loosened_simple_requirements(filter=extras_packages["3d"]),
+        'matlab': reqs.get_loosened_simple_requirements(filter=extras_packages["matlab"])
     },
     packages=find_packages(exclude=("tests",)),
     classifiers=[
