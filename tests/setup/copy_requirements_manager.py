4db981626bea6f6d089969c1627d1282e6af7043
Owen Walton <owen@monolithai.com>
2021-11-02 14:34:27 +0000

    pinned pytest to dev packages

--- tests/setup/copy_requirements_manager.py
+++ tests/setup/copy_requirements_manager.py
@@ -1,20 +1,4 @@
-import pathlib
 from typing import List, Dict
-
-import setuptools
-from setuptools import find_packages
-from setuptools.command.build_py import build_py as build_py_orig
-
-
-# from setuptools import dist
-# dist.Distribution().fetch_build_eggs(['Cython==0.29'])
-# from Cython.Build import cythonize
-
-extras_packages = {
-    "flask": ["flask", "tensorflow", "boto3"],
-    "3d": ["pyvista", "gmsh"],
-    "matlab": ["scipy"]
-}
 
 
 class RequirementsManager:
@@ -107,68 +91,3 @@ class RequirementsManager:
         """
         return [package for package in self.pipfile[self.package_index + 1:self.dev_package_index - 1] if
                 package.split(" = ")[1].startswith('{')]
-
-
-class CustomBuildPy(build_py_orig):
-    """
-    subclass build_py so that we collect no .py files inside the built pip package
-    this is done by overriding build_packages method with a noop
-    """
-    def build_packages(self):
-        pass
-
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-with open(str(pathlib.Path(__file__).parent.absolute()) + "/monolith_filemanager/version.py", "r") as fh:
-    version = fh.read().split("=")[1].replace("'", "")
-
-directives = {
-    'language_level': 3,
-    'always_allow_keywords': True
-}
-
-reqs = RequirementsManager(pipfile_loc="./Pipfile")
-reqs.set_package_dicts()
-
-# handle generation of install_requires filtered packages
-extra_packs = [item for sublist in extras_packages.values() for item in sublist]
-install_requires = [pack.split(">=")[0] for pack in reqs.get_loosened_requirements() if pack not in extra_packs]
-
-setuptools.setup(
-    name="monolith_filemanager",
-    version=version,
-    author="Maxwell Flitton",
-    author_email="maxwell@gmail.com",
-    description="Python package for reading and writing files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/MonolithAILtd/monolith-filemanager",
-    install_requires=install_requires,
-    extras_require={
-        'flask': reqs.get_loosened_requirements(filter=extras_packages["flask"]),
-        '3d': reqs.get_loosened_requirements(filter=extras_packages["3d"]),
-        'matlab': reqs.get_loosened_requirements(filter=extras_packages["matlab"])
-    },
-    packages=find_packages(exclude=("tests",)),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'file-hello = monolith_filemanager.console_commands.hello:print_logo',
-            'file-install-flask = monolith_filemanager.console_commands.install_flask:install_flask',
-            'file-install-tensorflow = monolith_filemanager.console_commands.install_tensorflow:install_tensorflow',
-            'file-install-aws = monolith_filemanager.console_commands.install_boto:install_boto'
-        ],
-    }
-    # ext_modules=cythonize("caching/**/*.py", exclude="tests/**/*.py", compiler_directives=directives, nthreads=4),
-    # cmdclass={'build_py': CustomBuildPy},
-    # include_package_data=False,
-    # options={"bdist_wheel": {"universal": "1"}}
-)
---
+++ tests/setup/test_setup_package_helper.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+from tests.setup.copy_requirements_manager import RequirementsManager
+
+extras_packages = {
+    "flask": ["flask", "tensorflow", "boto3"],
+    "3d": ["pyvista", "gmsh"],
+    "matlab": ["scipy"]
+}
+
+"""
+This test is included purely to demonstrate how the TestRequirementsManager class is working and that it correctly parses
+a Pipfile into a format suitable for the setup.py install_requires and extras_require package list args.
+"""
+
+all_packages = ['globre>=0.1.5', 'h5py>=2.10.0', 'joblib>=0.15.0', 'flask>=1.1.2', 'gmsh>=4.8.4', 'tensorflow>=2.2.0',
+                'numpy>=1.16.4', 'pandas>=0.25.1', 'distributed>=2021.1.1', 'dill>=0.2.9', 'pyvista>=0.24.2',
+                'pyyaml>=5.1.2', 'boto3>=1.10.5', 'botocore>=1.13.5', 'requests>=2.22.0', 'openpyxl>=3.0.7',
+                'pyarrow>=0.16.0', 'xlwt>=1.3.0', 'xlrd>=1.2.0', 'scipy>=1.4.1', 'dask[complete]>=2020.12.0']
+
+
+class TestRequirementsManager(TestCase):
+
+    def setUp(self) -> None:
+        self.reqs = RequirementsManager(pipfile_loc="tests/resources/Pipfile")
+        self.reqs.set_package_dicts()
+        self.packs = [item for sublist in extras_packages.values() for item in sublist]
+        self.fin_packs = [pack.split(">=")[0] for pack in self.reqs.get_loosened_requirements() if pack not in self.packs]
+
+    def test_manager_output(self):
+        self.assertEqual(all_packages, self.reqs.get_loosened_requirements(filter=self.fin_packs))
+        self.assertEqual(["flask>=1.1.2", "tensorflow>=2.2.0", "boto3>=1.10.5"], self.reqs.get_loosened_requirements(
+            filter=extras_packages["flask"]))
+        self.assertEqual(["gmsh>=4.8.4", "pyvista>=0.24.2"], self.reqs.get_loosened_requirements(filter=extras_packages["3d"]))
+        self.assertEqual(["scipy>=1.4.1"], self.reqs.get_loosened_requirements(filter=extras_packages["matlab"]))
+
