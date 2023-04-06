17ea98c542faec8bb97d8bfd8ac14d8e3dd90d50
Æsc <aesc.george@monolithai.com>
2023-04-06 10:33:15 +0100

    [MN-6724] Replace globre with Monolith implementation

--- tests/requirements_manager/test_requirements_manager.py
+++ tests/requirements_manager/test_requirements_manager.py
@@ -14,7 +14,7 @@ flask_packages = ["flask>=1.1.2", "tensorflow>=2.2.0", "boto3>=1.10.5"]
 three_d_packages = ["gmsh>=4.8.4", "pyvista>=0.24.2"]
 matlab_packages = ["scipy>=1.4.1"]
 
-all_packages = ['globre>=0.1.5', 'h5py>=2.10.0', 'joblib>=0.15.0', 'numpy>=1.16.4', 'pandas>=0.25.1',
+all_packages = ['h5py>=2.10.0', 'joblib>=0.15.0', 'numpy>=1.16.4', 'pandas>=0.25.1',
                 'distributed>=2021.1.1', 'dill>=0.2.9', 'pyyaml>=5.1.2', 'boto3>=1.10.5', 'botocore>=1.13.5',
                 'requests>=2.22.0', 'openpyxl>=3.0.7', 'pyarrow>=0.16.0', 'xlwt>=1.3.0', 'xlrd>=1.2.0',
                 'dask[complete]>=2020.12.0'] + flask_packages + three_d_packages + matlab_packages
