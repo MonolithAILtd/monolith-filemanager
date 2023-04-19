a54977d8303680bf66253177a8b8fef656be4a81
Æsc <aesc.george@monolithai.com>
2023-04-19 15:04:36 +0100

    [MN-6724] Downgrade pyvista

--- tests/requirements_manager/test_requirements_manager.py
+++ tests/requirements_manager/test_requirements_manager.py
@@ -12,7 +12,7 @@ extras_packages = {
 
 flask_packages = ["flask>=1.1.4", "tensorflow>=2.2.0", "boto3>=1.10.5", "protobuf>=3.20.1",
                   "jinja2>=2.11.3", "itsdangerous>=1.1.0", "werkzeug>=1.0.1"]
-three_d_packages = ["gmsh>=4.9.0", "pyvista>=0.38.5"]
+three_d_packages = ["gmsh>=4.9.0", "pyvista>=0.33.2"]
 matlab_packages = ["scipy>=1.4.1"]
 
 all_packages = ['h5py>=2.10.0', 'joblib>=0.15.0', 'numpy>=1.16.4', 'pandas>=0.25.1',
