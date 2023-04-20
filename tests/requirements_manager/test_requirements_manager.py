dc112e854cd4d59945b309ee69e588b4437ad1c8
Æsc <aesc.george@monolithai.com>
2023-04-20 10:24:33 +0100

    [MN-6724] Attempt to fix tests

--- tests/requirements_manager/test_requirements_manager.py
+++ tests/requirements_manager/test_requirements_manager.py
@@ -6,13 +6,13 @@ from requirements_manager.errors import NoPackagesInPipfileError, PipfilePathDoe
 
 extras_packages = {
     "flask": ["flask", "tensorflow", "boto3", "protobuf", "jinja2", "itsdangerous", "werkzeug",
-              "MarkupSafe"],
+              "markupsafe"],
     "3d": ["pyvista", "gmsh"],
     "matlab": ["scipy"]
 }
 
 flask_packages = ["flask>=1.1.4", "tensorflow>=2.2.0", "boto3>=1.10.5", "protobuf>=3.20.1",
-                  "jinja2>=2.11.3", "itsdangerous>=1.1.0", "werkzeug>=1.0.1", "MarkupSafe>=2.0.1"]
+                  "jinja2>=2.11.3", "itsdangerous>=1.1.0", "werkzeug>=1.0.1", "markupsafe>=2.0.1"]
 three_d_packages = ["gmsh>=4.9.0", "pyvista>=0.34.2"]
 matlab_packages = ["scipy>=1.4.1"]
 
