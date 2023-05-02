da1421d9b3bbb5729ebda21af7575085dca3f810
Æsc <aesc.george@monolithai.com>
2023-05-02 10:46:53 +0100

    [MN-6724] Remove xlrd - unused

--- tests/requirements_manager/test_requirements_manager.py
+++ tests/requirements_manager/test_requirements_manager.py
@@ -18,7 +18,7 @@ matlab_packages = ["scipy>=1.4.1"]
 
 all_packages = ['h5py>=2.10.0', 'joblib>=0.15.0', 'numpy>=1.22.4', 'pandas>=1.5.3',
                 'distributed>=2021.1.1', 'dill>=0.2.9', 'pyyaml>=5.1.2', 'boto3>=1.10.5', 'botocore>=1.13.5',
-                'requests>=2.22.0', 'openpyxl>=3.0.7', 'pyarrow>=1.0.1', 'xlwt>=1.3.0', 'xlrd>=2.0.1',
+                'requests>=2.22.0', 'openpyxl>=3.0.7', 'pyarrow>=1.0.1', 'xlwt>=1.3.0',
                 'dask[complete]>=2020.12.0'] + flask_packages + three_d_packages + matlab_packages
 
 
