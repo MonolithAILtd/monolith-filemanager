d69b1ea38a8d2c1e718aa78a8c406fdc39841ce1
tobygodwin <tobygodwin1@gmail.com>
2024-01-19 14:23:24 +0000

    wipp: remove compute for partial loading dask parquet

--- monolith_filemanager/file/pandas_file.py
+++ monolith_filemanager/file/pandas_file.py
@@ -212,9 +212,7 @@ class PandasFile(File):
             )
 
         if self.path.file_type == "parquet":
-            self.DASK_LOADING_METHODS[self.path.file_type](self.path).head(
-                row_end
-            ).compute()
+            self.DASK_LOADING_METHODS[self.path.file_type](self.path).head(row_end)
         else:
             skip_rows = max(row_start - 1, 0)  # Adjust for zero-based index
             nrows = row_end - row_start
